0b27d91f05cb03e6c675e6684d7577cc171f3002
yellowb <122517167@qq.com>
2018-12-13 14:35:52 +0800

    edit

--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
 module apiserver
-
-require github.com/chromedp/chromedp v0.1.2
--- main.go
+++ main.go
@@ -3,50 +3,11 @@
 package main
 
 import (
-	"context"
-	"log"
+	"fmt"
 	"time"
-
-	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	var err error
-
-	// create context
-	ctxt, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// create chrome instance
-	c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// run task list
-	err = c.Run(ctxt, click())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// shutdown chrome
-	err = c.Shutdown(ctxt)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// wait for chrome to finish
-	err = c.Wait()
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(time.Now())
 }
 
-func click() chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(`https://golang.org/pkg/time/`),
-		chromedp.WaitVisible(`#footer`),
-		chromedp.Click(`#pkg-overview`, chromedp.NodeVisible),
-		chromedp.Sleep(150 * time.Second),
-	}
-}
