51bb8c400ab6b54a600c1ca78834e812f5106063
yellowb <122517167@qq.com>
2018-12-17 14:44:23 +0800

    edit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"apiserver/config"
+	"apiserver/model"
 	"errors"
 	"github.com/lexkong/log"
 	"github.com/spf13/pflag"
@@ -25,6 +26,10 @@ func main() {
 	if err := config.Init(*cfg); err != nil {
 		panic(err)
 	}
+
+	// init DB
+	model.DB.Init()
+	defer model.DB.Close()
 
 	// Set gin mode.
 	gin.SetMode(viper.GetString("runmode"))
--- model/init.go
+++ model/init.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/lexkong/log"
 	"github.com/spf13/viper"
 )
 
@@ -11,10 +14,47 @@ type Database struct {
 
 var DB *Database
 
-
-
 func setupDB(db *gorm.DB) {
 	db.LogMode(viper.GetBool("gormlog"))
 	db.DB().SetMaxOpenConns(20000)
 	db.DB().SetMaxIdleConns(0)
+}
+
+func openDB(username, password, addr, name string) *gorm.DB {
+	config := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=%t&loc=%s",
+		username,
+		password,
+		addr,
+		name,
+		true,
+		"Local")
+
+	db, err := gorm.Open("mysql", config)
+	if err != nil {
+		log.Errorf(err, "Database connection failed. Database name: %s", name)
+	}
+
+	setupDB(db)
+	return db
+}
+
+func InitSelfDB() *gorm.DB {
+	return openDB(viper.GetString("docker_db.username"),
+		viper.GetString("docker_db.password"),
+		viper.GetString("docker_db.addr"),
+		viper.GetString("docker_db.name"))
+}
+
+func GetSelfDB() *gorm.DB {
+	return InitSelfDB()
+}
+
+func (db *Database) Init() {
+	DB = &Database{
+		Self: GetSelfDB(),
+	}
+}
+
+func (db *Database) Close() {
+	_ = DB.Self.Close()
 }
